a4cedda4e38f4e4abacf7284c96912266d5b272f
dotbillu <billudestroyer@outlook.com>
2025-09-04 17:44:51 +0530

    http routes configured

---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import "dotenv/config"; 
+
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ error: `token not found pls signup/signin` });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
+    (req as any).user = decoded;
+    next();
+  } catch (e) {
+    return res.status(401).json({ error: `invalid token` });
+  }
+};
+
